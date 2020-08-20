4267a420892b2fa56fa6e7621ac1cdff7a178ed0
Hoài Phúc <toaitoai35@gmail.com>
2020-08-20 23:25:15 +0700

    lan4

--- js/script.js
+++ js/script.js
@@ -10,21 +10,3 @@ function scrollFunction() {
         
     }
 }
-
-$(".carousel").owlCarousel({
-    margin: 20,
-    responsive: {
-      0:{
-        items:1,
-        nav: false
-      },
-      600:{
-        items:2,
-        nav: false
-      },
-      1000:{
-        items:3,
-        nav: false
-      }
-    }
-  });
