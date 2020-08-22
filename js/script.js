7b4e679a746db5358b0026894f01fb59d66adc5d
Hoài Phúc <toaitoai35@gmail.com>
2020-08-23 00:57:51 +0700

    lan5

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
+
+
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
 
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("navbar").style.padding = "40px 0px";
-
+        document.getElementById("logo").style.height = "50%"
     } else {
         document.getElementById("navbar").style.padding = "50px 0px";
-        
+        document.getElementById("logo").style.height = "60%"
     }
-}
+};
+
